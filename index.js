0ea7047aaf63f369e8ec14c112c5d2752bb5b8fe
David Almonacid <david.almonacidf@gmail.com>
2022-06-06 18:54:30 -0500

    Show card with pokemon picture

--- index.js
+++ index.js
@@ -1,25 +1,56 @@
-function getRandomNumber(min, max) {
+const getRandomNumber = (min, max) => {
   let randomNumber = Math.round(Math.random() * (max - min)) + min;
 
   if (randomNumber === min) randomNumber++;
   else if (randomNumber === max) randomNumber--;
 
   return randomNumber;
-}
+};
 
-// https://pokeapi.co/api/v2/pokemon?limit=108&offset=386
+const formatName = (name, id) => {
+  const words = name.split('-');
+  const capitalizedWords = words.map(
+    (word) => word[0].toUpperCase() + word.slice(1)
+  );
+  const newName = capitalizedWords.join(' ');
+
+  if (id === 413) return newName + ' Cloak';
+  else if (id === 439) return newName + '.';
+  else if (id === 474) return newName.replace(' ', '-');
+  else if (id === 487 || id === 492) return newName + ' Forme';
+
+  return newName;
+};
+
 const fetchData = async (id) => {
   try {
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     const data = await res.json();
-    console.log(data);
+    drawCard(data, id);
   } catch (error) {
     console.log(error);
   }
 };
 
+const drawCard = (pokemon, pokemonNumber) => {
+  console.log(pokemon); // Remove
+
+  const main = document.querySelector('.main-content');
+  const template = document.getElementById('template-card').content;
+  const templateClone = template.cloneNode(true);
+  const fragment = document.createDocumentFragment();
+  const pokemonName = formatName(pokemon.name, pokemonNumber);
+
+  const image = templateClone.querySelector('.card__pokemon-picture');
+  image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonNumber}.png`;
+  image.alt = pokemonName;
+
+  fragment.appendChild(templateClone);
+  main.appendChild(fragment);
+};
+
 document.addEventListener('DOMContentLoaded', () => {
-  const randomPokemon = getRandomNumber(385, 496);
-  console.log(randomPokemon);
+  const randomPokemon = getRandomNumber(386, 495);
+  console.log(randomPokemon); // Remove
   fetchData(randomPokemon);
 });
